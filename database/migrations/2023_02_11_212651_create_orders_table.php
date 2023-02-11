583df39cdb50e0f1a632ebe9cef09985888fac6f
AslanAV <autlevaslan@yandex.ru>
2023-02-12 00:45:08 +0300

    add breeze authorization

---
+++ database/migrations/2023_02_11_212651_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('phone_number')->nullable();
+            $table->string('email');
+            $table->json('address')->nullable();
+            $table->foreignId('good_id')->constrained('goods');
+            $table->integer('sum_of_order');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};

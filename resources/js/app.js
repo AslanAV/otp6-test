d4bfe7026092ca73257a08c04ad58beba2c3e106
AslanAV <autlevaslan@yandex.ru>
2023-02-13 00:18:57 +0300

    add flash message and alert message

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,10 @@ import * as bootstrap from 'bootstrap';
 import './bootstrap';
 
 import '../sass/app.scss'
-
+import ujs from '@rails/ujs';
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+ujs.start();
